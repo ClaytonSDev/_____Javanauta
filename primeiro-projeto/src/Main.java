c1aed740d615841e0e819a4b2ed325dab07fe160
projetos <claytonsalesdev@gmail.com>
2024-11-11 08:21:48 -0300

    .
    .

---
+++ primeiro-projeto/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int idade = 25;
+        long quantidadeCachorros = 100;
+        double salarioMensal = 4500.00;
+        char genero = 'M';
+        boolean empregadoJava = true;
+
+        System.out.println("Hello world!");
+        System.out.println("Idade: " + idade);
+        System.out.println("Cachorros: " + quantidadeCachorros);
+        System.out.println("Salário: " + salarioMensal);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Empregado: " + empregadoJava);
+    }
+}
